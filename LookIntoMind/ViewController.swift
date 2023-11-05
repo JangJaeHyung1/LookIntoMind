aff1c68398faf32f24807d4ab3a831a780f2bf5f
jangjaehyung1 <drezzed7@gmail.com>
2023-11-05 19:32:22 +0900

    Initial Commit

---
+++ LookIntoMind/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LookIntoMind
+//
+//  Created by jh on 2023/11/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
